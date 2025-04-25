ab36684e85fcf87347d475110705342014c4c10c
xqzpop7 <xqzpop7@pm.me>
2025-04-25 14:28:13 +0200

    Huge Update, not telling you what I did :3

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
+import UnderConstructionGif from './assets/Under Construction.gif'
+import CodingGif from './assets/Coding.gif'
+
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <img className="Gifs" src={ UnderConstructionGif } alt='Text "Under Construction" with an animated background.' ></img>
+      <img className="Gifs" src={ CodingGif } alt='Stickman banging his head into a keyboard on a construction sign.' ></img>
     </>
   )
 }
